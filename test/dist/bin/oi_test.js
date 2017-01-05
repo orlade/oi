c4277c67d6ad0da3e353b1a687caff15629064da
Oliver Lade <piemaster21@gmail.com>
2017-01-05 21:19:33 +1100

    Use Chai to assert CLI

--- test/dist/bin/oi_test.js
+++ test/dist/bin/oi_test.js
@@ -2,13 +2,13 @@
 
 import {exec} from '../../../src/utils/util';
 import assert from 'assert';
+require('chai').should();
 
 describe("Oi CLI", () => {
 
   it("can be invoked", () => {
     const result = exec('node dist/bin/oi.js -v');
-    console.log(result);
-    assert(result.stdout.trim().match(/\d+\.\d+\.\d+/), "Invalid version output");
+    result.stdout.trim().should.match(/^\d+\.\d+\.\d+$/);
   });
 
 });
