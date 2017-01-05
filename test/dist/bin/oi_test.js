524af37ceec9e66ff3fa5b77bbbbd28e172787ea
Oliver Lade <piemaster21@gmail.com>
2017-01-05 21:40:00 +1100

    More logging in test

--- test/dist/bin/oi_test.js
+++ test/dist/bin/oi_test.js
@@ -8,6 +8,12 @@ describe("Oi CLI", () => {
 
   it("can be invoked", () => {
     const result = exec('node dist/bin/oi.js -v');
+    console.log(result);
+    console.log(result.toString());
+    console.log(result.code);
+    console.log(result.stdout);
+    console.log(result.stdout.trim());
+    console.log(result.stdout.trim().match(/^\d+\.\d+\.\d+$/));
     result.stdout.trim().should.match(/^\d+\.\d+\.\d+$/);
   });
 
