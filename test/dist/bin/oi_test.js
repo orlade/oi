32e04a27e001e34dade59bf9c63df398285e9b8b
Oliver Lade <piemaster21@gmail.com>
2017-01-05 21:56:40 +1100

    Make test asynchronous if it wants to be

--- test/dist/bin/oi_test.js
+++ test/dist/bin/oi_test.js
@@ -6,7 +6,7 @@ require('chai').should();
 
 describe("Oi CLI", () => {
 
-  it("can be invoked", () => {
+  it("can be invoked", (done) => {
     const result = exec('node dist/bin/oi.js -v');
     console.log(result);
     console.log(result.toString());
@@ -16,8 +16,10 @@ describe("Oi CLI", () => {
     console.log(result.stdout.trim().match(/^\d+\.\d+\.\d+$/));
     try {
       result.stdout.trim().should.match(/^\d+\.\d+\.\d+$/);
+      done();
     } catch (e) {
       console.error('ERROR:', e);
+      done(e);
       throw Error('FAIL');
     }
   });
